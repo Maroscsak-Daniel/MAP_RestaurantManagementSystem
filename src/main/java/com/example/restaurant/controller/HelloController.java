530b0b0e41b37625185005b4a8561a511c3f9810
Maroscsak Daniel <daniel.maroscsak@gmail.com>
2025-10-25 21:19:20 +0300

    Setup completion x2

--- src/main/java/com/example/restaurant/controller/HelloController.java
+++ src/main/java/com/example/restaurant/controller/HelloController.java
@@ -1,4 +1,13 @@
 package com.example.restaurant.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Die Anwendung funktioniert!";
+    }
 }
